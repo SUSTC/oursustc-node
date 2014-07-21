c41bcab07ae8edc8b3c3a4103cafb85feb6afcf0
ProfFan <admin@amayume.net>
2014-07-21 13:54:50 +0800

    Tree build implemented

--- src/control/board.js
+++ src/control/board.js
@@ -11,9 +11,9 @@
 
   var Topic = require("./../class/topic").Topic;
 
-  /*BoardProxy.newAndSave('bla', 'baba', 0, 0, '', null, function (err, b) {
+  /* BoardProxy.newAndSave('bla', 'baba', 0, 0, '', '53c908582ccde07c15000022', function (err, b) {
     console.log(err, b);
-  });*/
+  }); */
 
   /*function initData(res, data, subtitle) {
     if (subtitle) {
@@ -33,33 +33,55 @@
       var topic = new Topic(req.params.shortcut);
       topic.index(req, res, data, callback);
     } else {
-      //TODO: index, show all boards
+      // All preprocessing completed, Render the page
       var e = EventProxy.create(['render'], function(status) {
         data.active = 'board';
-        console.log('[BOARD] Callback Executed');
         callback();
 
       });
-
+      // Tree has been built, Do preprocessing here PLS
       e.once('treebuilt', function(tree) {
-        console.log('[BOARD] Index Rendered');
+        var record = tree; //This is for debugging only
+        /*
+        var dumptree = function(tree, level) {
+          if (tree)
+            for (var i in tree) {
+              console.log(level, tree[i]._id);
+              dumptree(tree[i].children, level + 1);
+            }
+        }
+        dumptree(tree, 1); // For debugging
+        */
         e.emit('render', record)
       });
-      console.log("aaa");
+      //Here we build the tree structure BEGIN
       BoardProxy.fetchAll(function(err, list) {
-        tree={};
+        var tree = [];
+        // build function BEGIN
         var build = function(parent) {
+          var current = parent;
+          current.children = [];
 
+          for (var i = 0; i < list.length; i++) {
+            if ((typeof(list[i].parent) !== 'undefined') && (list[i].parent == parent._id.toString())) {
+              var child = build(list[i]);
+              current.children[child._id] = child;
+            }
+          }
+          return current;
         };
-        for(var i=0;i<list.length;i++){
-          if(ibuild(list[i]._id);
-          console.log(list[i]);
+        // build function END
+
+        for (var i = 0; i < list.length; i++) {
+          if (list[i].parent == null) {
+            var child = build(list[i]);
+            tree[child._id] = child;
+          }
         }
-        console.log('[BOARD] Tree Built');
-        e.emit('treebuilt', list);
+        e.emit('treebuilt', tree);
 
       });
-
+      // Tree building END
     }
 
   };
